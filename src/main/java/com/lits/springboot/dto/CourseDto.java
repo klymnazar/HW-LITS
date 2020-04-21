c37a99454aba748e6d5d3fa667854b9792c00549
klymnazar <klymnazar@gmail.com>
2020-04-21 16:32:04 +0300

    First commit

--- src/main/java/com/lits/springboot/dto/CourseDto.java
+++ src/main/java/com/lits/springboot/dto/CourseDto.java
@@ -1,62 +1,20 @@
 package com.lits.springboot.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDate;
 import java.util.List;
 
+@Getter
+@Setter
 public class CourseDto {
     private Integer id;
     private String courseName;
     private Integer teacherId;
     private LocalDate startDate;
     private LocalDate endDate;
-
+    private List<TeacherDto> teachers;
     private List<Integer> teacherIds;
 
-    public List<Integer> getTeacherIds() {
-        return teacherIds;
-    }
-
-    public void setTeacherIds(List<Integer> teacherIds) {
-        this.teacherIds = teacherIds;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getCourseName() {
-        return courseName;
-    }
-
-    public void setCourseName(String courseName) {
-        this.courseName = courseName;
-    }
-
-    public Integer getTeacherId() {
-        return teacherId;
-    }
-
-    public void setTeacherId(Integer teacherId) {
-        this.teacherId = teacherId;
-    }
-
-    public LocalDate getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(LocalDate startDate) {
-        this.startDate = startDate;
-    }
-
-    public LocalDate getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(LocalDate endDate) {
-        this.endDate = endDate;
-    }
 }
